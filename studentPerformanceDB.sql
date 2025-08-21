fc122b8a6ea833a7f4d1a39b88075720cc1c8ce4
Lennie02 <o.ishimwe1@alustudent.com>
2025-08-21 14:46:53 +0200

    update studentPerformanceDB.sql

--- studentPerformanceDB.sql
+++ studentPerformanceDB.sql
@@ -42,3 +42,24 @@ CREATE TABLE python_grades (
 -- Insert sample students (at least 15 total)
 -- ==========================================
 
+INSERT INTO students (student_name, intake_year) VALUES
+('Okechukwu Wisdom', 2025),
+('Lennie Ishimwele', 2025),
+('Nawaf Ahmad', 2025),
+('Cyuzuzuo Germain', 2025),
+('Willy Tech', 2025),
+('Divin Bonheur', 2025),
+('Grace Jafar', 2025),
+('Henry Maduabuchi', 2025),
+('Isabella Victor', 2025),
+('Blessing Okechukwu', 2025),
+('James John', 2025),
+('Naruto John', 2025),
+('Lee Chan', 2025),
+('Sam Altman', 2025),
+('Charles Daniel', 2025);
+
+-- ==================================
+-- Insert Linux course sample grades
+-- ==================================
+INSERT INTO linux_grades (student_id, grade_obtained) VALUES
